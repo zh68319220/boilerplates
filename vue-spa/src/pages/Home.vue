bb3cb61ef05cf13185ceac94e1255b77e01558be
欧阳德才 <584278919@qq.com>
2017-04-14 16:13:54 +0800

    add apis

--- vue-spa/src/pages/Home.vue
+++ vue-spa/src/pages/Home.vue
@@ -1,15 +1,14 @@
 <template>
 <div class="app">
   <h1>Home</h1>
-  <p>{{data.email}}</p>
-  <p>{{data.blog}}</p>
+  <p>{{data.login}}</p>
   <Footer-menu></Footer-menu>
 </div>
 </template>
 
 <script type='es6'>
 import Footer from '../components/Footer.vue'
-import { getGitUser } from '../actions/'
+import { getGitUser } from '../api/'
 
 export default {
   name: 'home',
@@ -24,6 +23,10 @@ export default {
   mounted () {
     var _this = this
     getGitUser('zh68319220').then((data) => {
+      return getGitUser('yyx990803')
+    }).then((data) => {
+      return getGitUser('yursile')
+    }).then((data) => {
       _this.data = data
     })
   }
